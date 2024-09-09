a649ee509053eb0620a1397c7575776e5f2b7cb5
ohnoruu <rubydao007@gmail.com>
2024-09-09 11:22:12 -0700

    stuff

--- Problem0.java
+++ Problem0.java
@@ -1,7 +1,31 @@
+/*
+ * ## Order of Operations Practice
+**Instructions:** Evaluate the following expressions.  You can answer either in this README file, in your own notebook, or on a separate piece of paper.
+### Part 1
+a. 3 * (7 - 4) + 6 / 2
+
+b. ((12 / 3) * (7 - 5) + 4) / 2
+
+c. (5 + 2) * (8 - 3) % 2
+
+d. (15 % 4) + (5 / 2) - (3 * 2)
+
+e. 5 * 2 + 4 / 2 % 3
+
+f. (18 - 12) % 3 * (5 - 3 + 2)
+
+g. 6 % (8 - 6 + 2) - (3 * 3) / 3
+
+h. (12 % (5 / 2)) * 3 + (7 - 4)
+
+i. (14 / 2) * 3 + 6 % 2
+
+j. ((15 % 3) / 3) * (7 - 4) + 5
+ */
 public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    
   }
 }
